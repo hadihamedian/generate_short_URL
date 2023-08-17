de95985c921d812970521370b0dcf7e8a1236ac8
Hadi Hamedian <hamedianhadi@live.com>
2023-08-17 10:59:12 +0330

    Add Url Controller

---
+++ ShortLink/ShortLink/Controllers/UrlController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShortLink.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlController : ControllerBase
+    {
+    }
+}
