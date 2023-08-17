5ffb65daccb2c49830b2188d3f790adeef7a6738
Hadi Hamedian <hamedianhadi@live.com>
2023-08-17 19:07:08 +0330

    Add-Migration and Create Database

--- ShortLink/src/2.Infra/ShortLink.Infra.SQL/LinkDbContext.cs
+++ ShortLink/src/2.Infra/ShortLink.Infra.SQL/LinkDbContext.cs
@@ -5,6 +5,12 @@ namespace ShortLink.Infra.SQL
 {
     public class LinkDbContext : DbContext
     {
+        public LinkDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<Link> Links { get; set; }
+
+     
     }
 }
---
+++ ShortLink/src/2.Infra/ShortLink.Infra.SQL/Migrations/20230817152151_init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShortLink.Infra.SQL.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShortUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumberOfVisit = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
